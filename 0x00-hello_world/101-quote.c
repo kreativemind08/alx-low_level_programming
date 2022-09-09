740c60ea2d34603d035edd0250a783be7ffe95fe
Samuel J <27231216+kreativemind08@users.noreply.github.com>
2022-09-09 16:16:10 +0100

    8. UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - print quote
  *
@@ -9,6 +11,6 @@
 
 int main(void)
 {
-	write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19n, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
