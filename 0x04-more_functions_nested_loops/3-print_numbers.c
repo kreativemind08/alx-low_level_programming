4094db7df50640ae52f5364005dd8c83251fe668
samj <27231216+kreativemind08@users.noreply.github.com>
2022-09-15 23:06:28 +0100

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: first integer
- * @b: second integer
+ * print_numbers - prints numbers 0-9
  *
- * Return: result (product of two integers)
+ * Return: void
  */
 
-int mul(int a, int b)
+void print_numbers(void)
 {
-	return (a * b);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(n);
 }
