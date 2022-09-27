f9ac983ecbb817fa09f7afd638180bf8dce17b67
samj <27231216+kreativemind08@users.noreply.github.com>
2022-09-27 23:23:26 +0100

    Task 6 i

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: chessboard
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: 2d char array representing board
+ *
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
@@ -14,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar((*(a + i))[j]);
 		}
 		_putchar(n);
 	}
